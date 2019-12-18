349fb46fa919d4a040ecfde591ee1854c1aa72f5
AndersonCMelo <andersonmelo13@gmail.com>
2019-12-17 21:43:17 -0300

    chore: Configura o Prettier e faz a integração com o ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,18 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    extends: "airbnb-base",
+    extends: ['airbnb-base', 'prettier'],
+    plugins: ['prettier'],
     globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
     },
-    parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaVersion: 2018,
-        sourceType: "module"
+        sourceType: 'module'
     },
-    plugins: [
-        "@typescript-eslint"
-    ],
     rules: {
+        "prettier/prettier": "error",
         "class-methods-use-this": "off",
         "no-param-reassign": "off",
         "camelcase": "off",
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@ const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ messege: 'Hello World' });
-})
+});
 
 export default routes;
