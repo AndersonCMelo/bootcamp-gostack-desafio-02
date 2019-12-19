8bff40d43d64b0d1cd09747efb35dcc1792e5c88
AndersonCMelo <andersonmelo13@gmail.com>
2019-12-18 21:53:18 -0300

    chore: Corrige a configuração de porta no arquivo database.js e faz a primeira migration de usuário para criar a tabale users

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,15 @@
+// Configuração de conexão com o banco de dados
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: '192.168.99.100',
   username: 'postgres',
   password: 'gympassword',
   database: 'gympoint',
+  port: '5433',
   define: {
+    // Atribui a data de criação e edição nas colunas do banco de dados
     timestamps: true,
+    // Caixa baixa separada por "_". Exemplo: user_groups
     underscored: true,
     underscoredAll: true,
   },
