418a46a8f280e0fcefcee666bf04f3cc7c353d3c
AndersonCMelo <andersonmelo13@gmail.com>
2019-12-18 23:50:22 -0300

    chore: Ajeita o script do nodemon no package.json, cria o loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ messege: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Anderson Melo',
+    email: 'andersonmelo13@gmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
