14ea85c57b5205d208b0a03b43a391020a65c1f7
AndersonCMelo <andersonmelo13@gmail.com>
2019-12-22 13:20:12 -0300

    chore: Cria a tabela students por meio de uma migration

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 import { Router } from 'express';
-
+// Coneção entre o front-end e o back-end do body para o banco
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+// Middlewares
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+// Rota para cadastro de usuários
 routes.post('/users', UserController.store);
+
+// Rota para iniciar uma sessão
 routes.post('/sessions', SessionController.store);
 
+// Middleware para autenticação de rotas
 routes.use(authMiddleware);
 
+// Rota para atualização de informaççoes do usuário
 routes.put('/users', UserController.update);
 
 export default routes;
