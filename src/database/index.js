136f2d44f7ac015316b4b53983b7ac7cba3a8220
AndersonCMelo <andersonmelo13@gmail.com>
2019-12-22 14:20:24 -0300

    chore: Configura o loader dos models para receber o model Student

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,28 @@
+// Cria o Loader para os models
+
 import Sequelize from 'sequelize';
 
+// Importa o arquivo responsável pela manipulação das informações do banco para cada usuário
 import User from '../app/models/User';
+
+// Importa o arquivo responsável pela manipulação das informações dos estudantes
+import Student from '../app/models/Student';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+// Cria um vetor com os models da aplicação
+const models = [User, Student];
 
 class Database {
   constructor() {
     this.init();
   }
 
+  // Faz a conexão com a base de dados
   init() {
     this.connection = new Sequelize(databaseConfig);
 
+    // Conecta cada model (User e Student) ao banco de dados
     models.map(model => model.init(this.connection));
   }
 }
